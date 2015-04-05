44b7b4839d36a347887bbd9166d3b6a25fef70b6
Moisés Pérez <mxpxgx@gmail.com>
2015-04-05 10:42:27 -0700

    Add home app

--- moiprezsite/urls.py
+++ moiprezsite/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from home.views import HomeView
 
 urlpatterns = [
     # Examples:
-    # url(r'^$', 'moiprezsite.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+    # url(r'^$', 'home.views.HomeView', name='home'),
+    url(r'^$', HomeView.as_view(), name='home'),
+    # url(r'^home/', include('home.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls))
 ]
